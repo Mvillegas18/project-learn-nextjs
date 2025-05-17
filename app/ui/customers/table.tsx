cd34249067e4588dde24c5d4bc6996f93b644f96
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-05-17 11:32:50 -0500

    Refactor code structure for improved readability and maintainability

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,7 +1,7 @@
 import { FormattedCustomersTable } from '@/app/lib/definitions';
-import { lusitanaFont } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
 import Image from 'next/image';
+import { lusitanaFont } from '../fonts';
 
 export default async function CustomersTable({
 	customers,
