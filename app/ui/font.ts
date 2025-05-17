b689d74a591e1ea46879d31a0ca81b3376cd365e
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-05-16 19:48:41 -0500

    Add font imports and apply styles to layout and page components

---
+++ app/ui/font.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const interFont = Inter({ subsets: ['latin'] });
+export const lusitanaFont = Lusitana({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+});
